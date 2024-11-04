570039a77185774e044fe86bd1d5006cdd2942da
JASHWANTH <120658213+jashwanth310@users.noreply.github.com>
2024-11-04 18:04:12 +0530

    emp_bonus.py

---
+++ FUNCTIONS/Employee Assingnment/emp_bonus.py
@@ -0,0 +1,11 @@
+#emp_module consist of user defined functions definitions
+#related employees
+def emp_total_no():
+    total_no_emp=200
+    print("Total no.of current employees: ",total_no_emp)
+def emp_details():
+    list1=["NANI","PRASAD","REDDY","srini"]
+    print("Empolyees Names:",list1)
+def emp_bonus_details(sal):
+    bonus=(sal*15)/100
+    print("Emp Bonus:",bonus)
