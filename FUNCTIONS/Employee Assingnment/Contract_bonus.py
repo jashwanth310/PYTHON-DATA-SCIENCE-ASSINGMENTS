7d9401617c11213b62e0b45548df98c7f6b495d0
JASHWANTH <120658213+jashwanth310@users.noreply.github.com>
2024-11-04 18:02:42 +0530

    Create Contract_bonus.py

---
+++ FUNCTIONS/Employee Assingnment/Contract_bonus.py
@@ -0,0 +1,11 @@
+#emp_module consist of user defined functions definitions
+#related employees
+def emp_total_nos():
+    total_no_emp=20
+    print("Total no.of current employees: ",total_no_emp)
+def emp_details():
+    list1=["CHARAN","CHATGPT","BLACKBOX","GEMINI"]
+    print("Empolyees Names:",list1)
+def emp_bonus_details(sal):
+    bonus=(sal*10)/100
+    print("Emp Bonus:",bonus)
